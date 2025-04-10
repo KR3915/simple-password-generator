a4bf797a13d3c6c52c479f40fbcb879518a3677b
KR3915 <ondrej.brdek@gmail.com>
2025-04-10 20:31:49 +0200

    character generation

--- password generator.py
+++ password generator.py
@@ -1,6 +1,13 @@
 import random
+import string
+
 from string import ascii_uppercase
 def generate_chars(size, type):
-    chars = "".join(random.choice(chars) for i in range(size)
-generate_chars(2, string.ascii_uppercase + string.digits)
+    return "".join(random.choice(type) for i in range(size))
+upper = generate_chars(random.randint(1,3), string.ascii_uppercase)
+lower = generate_chars(random.randint(1,3), string.ascii_lowercase)
+numeral = generate_chars(random.randint(1,3), string.digits)
+special = generate_chars(1, "!@#$%^&*()_")
+
+print(upper, lower, numeral, special)
     
