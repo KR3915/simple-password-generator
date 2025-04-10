7aca5fe6d0f544a083256ede1d3b2328d53d7c83
KR3915 <ondrej.brdek@gmail.com>
2025-04-10 20:21:39 +0200

    base

---
+++ password generator.py
@@ -0,0 +1,6 @@
+import random
+from string import ascii_uppercase
+def generate_chars(size, type):
+    chars = "".join(random.choice(chars) for i in range(size)
+generate_chars(2, string.ascii_uppercase + string.digits)
+    
